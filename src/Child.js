ec51d7122abf7722120ca1c5bc2870140e5e3854
Novice2921 <appudecember@gmail.com>
2025-02-01 21:30:24 -0500

    CS 450 Assignment 1

---
+++ src/Child.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      inputText: '',
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ inputText: event.target.value });
+  };
+
+  addTodo = () => {
+    this.setState({ todos: [...this.state.todos, this.state.inputText], inputText: ''});
+  };
+  removeTodo = (task) => {
+    var filtered_task = this.state.todos.filter(item=>item !== task)
+    this.setState({ todos: filtered_task })
+  };
+
+
+
+  render() {
+    return (
+      <div style={{padding:20}}>
+        <input type="text" value={this.state.inputText} onChange={this.handleInputChange} placeholder="Add a todo" />
+        <button onClick={this.addTodo}>Add Todo</button>
+        {this.state.todos.map(item=><div><label>{item}</label> <button onClick={()=>this.removeTodo(item)}>Remove</button></div>)}
+      </div>
+    );
+  }
+}
+
+export default TodoList;
